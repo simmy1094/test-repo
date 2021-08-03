3965f34ea18a5da3d5d86e8f5537a86fe547c9c3
Smita Kumari <smitha.kumari@lloydsbanking.com>
2021-08-03 17:06:59 +0100

    added docker

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -1,7 +1,11 @@
 pipeline {
-  agent any
+  agent {
+        docker {
+            image 'node:lts-buster-slim' 
+            args '-p 3000:3000' 
+        }
+    }
 
-  
     stages {
         stage('Build') {
             steps {
